ff0c4eed6d14bd3571c17751385e639080fa9a40
bbbooo1994 <bbbooo1994@gmail.com>
2019-09-27 21:27:10 +0100

    v_0.1

---
+++ flask_app/run.py
@@ -0,0 +1,45 @@
+from flask import Flask
+from config import Configdb
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+from flask_migrate import Migrate
+from flask_restful import Api
+from flask_jwt_extended import JWTManager
+from flask_cors import CORS
+from flask_static_compress import FlaskStaticCompress
+
+app = Flask(__name__, static_folder="static", template_folder="templates")
+CORS(app)
+compress = FlaskStaticCompress(app)
+# app.config.from_object(Config)
+app.config.from_object(Configdb)
+
+app.config['JWT_SECRET_KEY'] = 'jwt-secret-string'
+app.config['JWT_BLACKLIST_ENABLED'] = True
+app.config['JWT_BLACKLIST_TOKEN_CHECKS'] = ['access', 'refresh']
+
+api = Api(app)
+jwt = JWTManager(app)
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
+migrate = Migrate(app, db)
+
+import models, resources, views
+
+api.add_resource(resources.SignUp, '/signup')
+api.add_resource(resources.Login, '/login')
+api.add_resource(resources.LogoutAccess, '/logout/access')
+api.add_resource(resources.LogoutRefresh, '/logout/refresh')
+api.add_resource(resources.TokenRefresh, '/token/refresh')
+api.add_resource(resources.UserLocation, '/location')
+api.add_resource(resources.SecretResource, '/secret')
+
+
+@jwt.token_in_blacklist_loader
+def check_if_token_in_blacklist(decrypted_token):
+    jti = decrypted_token['jti']
+    return models.RevokedTokenModel.is_jti_blacklisted(jti)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ flask_app/views.py
@@ -0,0 +1,8 @@
+from run import app
+from flask import render_template, url_for
+
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def catch_all(path):
+    return render_template("index.html")
